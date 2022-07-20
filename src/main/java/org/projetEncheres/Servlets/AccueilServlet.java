4bfc0500e7b459b8c4947f0e485828d2272c16d7
Claod <Claod@MSI>
2022-07-20 10:10:42 +0200

    Pool : Ok AccueilServlet Contains information about "TODO modifications", "Tips and Tricks concerning configurations", and the "pool's configuration to past inside the context.xml"

--- src/main/java/org/projetEncheres/Servlets/AccueilServlet.java
+++ src/main/java/org/projetEncheres/Servlets/AccueilServlet.java
@@ -12,6 +12,31 @@ import org.projetEncheres.DAL.TestDBPool;
 
 /**
  * Servlet implementation class AccueilServlet
+ * 
+ * MODIFICATIONS COTé SQL SERVEUR :
+ * Il faut aller dans SQL Server Configuration Manager (C:\Windows\System32\SQLServerManager15.msc) dans Configuration du réseau SQL server :
+ *  activer Cannaux nommés
+ *  Activer TCP/IP et aller dans l'onglet Adresse IP :
+ *  descendre tout en bas, supprimer la valeur du port dynamique et enfin ajouter la valeur 1433 au Port TCP juste au dessus.
+ *  
+ *  TODO SQL modifier la taille du varchar table UTILISATEURS colonne email à 50
+ *  TODO Voir pour la rue
+ *  
+ *  COPIER COLLER DANS context.xml au dessus de </context> et en modifiant le mot de passe dans le xml ci-dessous le cas échéant
+ *  
+ *  <Resource	name="jdbc/encherePool"
+       		driverClassName="com.microsoft.sqlserver.jdbc.SQLServerDriver"
+        		type="javax.sql.DataSource"
+        		
+        		
+        		url="jdbc:sqlserver://localhost;integratedSecurity=false;encrypt=true;trustServerCertificate=true;databasename=enchere;"
+        		username="sa"
+        		password="Pa$$w0rd"
+        		
+        		maxTotal="100"
+        		maxIdle="30"
+        		maxWaitMillis="10000" />
+ *  
  */
 public class AccueilServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -40,6 +65,13 @@ public class AccueilServlet extends HttpServlet {
 		doRequest(request, response);
 	}
 
+	/**
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 */
 	protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		TestDBPool test = new TestDBPool();
