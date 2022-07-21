5ef2df7460d42e794a8b08cf4b11d923d67abade
Claod <Claod@MSI>
2022-07-21 13:58:52 +0200

    Les tests de BDD se font maitnenant dans une servlet a part

--- src/main/java/org/projetEncheres/Servlets/AccueilServlet.java
+++ src/main/java/org/projetEncheres/Servlets/AccueilServlet.java
@@ -1,14 +1,12 @@
 package org.projetEncheres.Servlets;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import org.projetEncheres.BO.Utilisateur;
-import org.projetEncheres.DAL.TestDBPool;
 
 /**
  * Servlet implementation class AccueilServlet
@@ -77,25 +75,8 @@ public class AccueilServlet extends HttpServlet {
 	 * @throws IOException
 	 */
 	protected void doRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		TestDBPool test = new TestDBPool();
-		Utilisateur u = new Utilisateur();
-		u.setNom("Harivel");
-		u.setPrenom("Maxime");
-		u.setPseudo("Claod");
-		u.setEmail("maxime.harivel2016@campus-eni.fr");
-		u.setTelephone("0600000000");
-		u.setRue("Rue Tabaga");
-		u.setCodePostal("44100");
-		u.setVille("Nantes");
-		u.setMotDePasse("essaie");
-		u.setCredit(1000);
-		u.setIsAdministrateur(true);
-		try {
-			TestDBPool.insert(u);
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		RequestDispatcher dispatcher;
+		dispatcher = request.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp"); 
+		dispatcher.forward(request, response);
 	}
 }
