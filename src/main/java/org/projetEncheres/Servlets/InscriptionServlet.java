b596e5bfb9710ce18b61b93f39ff8b27b98bfa59
Claod <Claod@MSI>
2022-07-23 11:52:15 +0200

    Mise en place et configuration d'un firltre pour les personnes non connectées

--- src/main/java/org/projetEncheres/Servlets/InscriptionServlet.java
+++ src/main/java/org/projetEncheres/Servlets/InscriptionServlet.java
@@ -1,19 +1,13 @@
 package org.projetEncheres.Servlets;
 
-import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
-
 import java.io.IOException;
-import java.net.http.HttpRequest;
-
 import org.projetEncheres.BO.Utilisateur;
 import org.projetEncheres.DAL.DAOFactory;
-import org.projetEncheres.EXCEPTIONS.DALException;
 
 /**
  * Servlet implementation class InscriptionServlet
