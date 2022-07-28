e4c876e46342c7422f0110fdc51277136091701e
Claod <Claod@MSI>
2022-07-28 11:20:01 +0200

    Javadoc utilisateur v2

--- src/main/java/org/projetEncheres/BO/Utilisateur.java
+++ src/main/java/org/projetEncheres/BO/Utilisateur.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 /**
 *
-**@author Claod
+*@author Claod
 *{@summary Classe modélisant un utilisateur }
 */
 public class Utilisateur implements Serializable {
@@ -17,9 +17,16 @@ public class Utilisateur implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = -8472308215182534529L;
+	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété de la montant du crédit }
+	*/
 	private Integer credit;
 
 	/**
+	 * @author Claod
 	 * {@summary get The credit}
 	 * @return The Credit as Integer
 	 */
@@ -28,6 +35,7 @@ public class Utilisateur implements Serializable {
 	}
 	
 	/**
+	 * @author Claod
 	 * {@summary set The credit}
 	 * @param Integer amount
 	 */
@@ -35,116 +43,276 @@ public class Utilisateur implements Serializable {
 		this.credit = amount;
 	}
 
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du nom }
+	*/
 	private String nom;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getNom() {
 		return this.nom;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param Nom
+	 */
 	public void setNom(String Nom) {
 		this.nom = Nom;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du prenom }
+	*/
 	private String prenom;
 
 	public String getPrenom() {
 		return this.prenom;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du uméro de l'utilisateur }
+	*/
 	private Integer noUtilisateur;
 
 	public Integer getNoUtilisateur() {
 		return this.noUtilisateur;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param id
+	 */
 	public void setNoUtilisateur(Integer id) {
 		this.noUtilisateur = id;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param prenom
+	 */
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété représentant le role (administrateur ou non) de l'utilisateur }
+	*/
 	private Boolean administrateur;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public Boolean isAdministrateur() {
 		return this.administrateur;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param administrateur
+	 */
 	public void setIsAdministrateur(Boolean administrateur) {
 		this.administrateur = administrateur;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du Pseudo }
+	*/
 	private String pseudo;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getPseudo() {
 		return this.pseudo;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param pseudo
+	 */
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété de l'email }
+	*/
 	private String email;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getEmail() {
 		return this.email;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param email
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du téléphone }
+	*/
 	private String telephone;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getTelephone() {
 		return this.telephone;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param telephone
+	 */
 	public void setTelephone(String telephone) {
 		this.telephone = telephone;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété de la rue }
+	*/
 	private String rue;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getRue() {
 		return this.rue;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param rue
+	 */
 	public void setRue(String rue) {
 		this.rue = rue;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du code postal }
+	*/
 	private String codePostal;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getCodePostal() {
 		return this.codePostal;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param codePostal
+	 */
 	public void setCodePostal(String codePostal) {
 		this.codePostal = codePostal;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété de la ville }
+	*/
 	private String ville;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getVille() {
 		return this.ville;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param ville
+	 */
 	public void setVille(String ville) {
 		this.ville = ville;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Propriété du mot de passe }
+	*/
 	private String motDePasse;
 
+	/**
+	 * @author Claod
+	 * 
+	 * @return
+	 */
 	public String getMotDePasse() {
 		return this.motDePasse;
 	}
 
+	/**
+	 * @author Claod
+	 * 
+	 * @param motDePasse
+	 */
 	public void setMotDePasse(String motDePasse) {
 		this.motDePasse = motDePasse;
 	}
 	
+	/**
+	*
+	*@author Claod
+	*{@summary Constructeur vide }
+	*/
 	public Utilisateur() {
 		super();
 	}
